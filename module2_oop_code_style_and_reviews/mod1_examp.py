21ebe32b4b10b4fb976b8bed29974d4df5aa915c
indrejue <Antony.Farag1@gmail.com>
2020-10-30 12:53:55 -0400

    fixed formating

--- module2_oop_code_style_and_reviews/mod1_examp.py
+++ module2_oop_code_style_and_reviews/mod1_examp.py
@@ -1,17 +1,17 @@
 """ helper function for Data science """
 
-#importing libraries for function into the package
+# importing libraries for function into the package
 import pandas as pd
-import numpy as np 
+import numpy as np
+
 
 class MyDataFrame(pd.DataFrame):
-      
     # define a null count function
     def nulls(self):
         count = self.isnull().sum().sum()
         return count
 
-    #create a date split function.
+    # create a date split function.
     def date_split(self):
         self['col'] = self.select_dtypes('datetime')
         self['year'] = self['col'].dt.year
